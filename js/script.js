81f17366ff99cdbefc18c104bb05985885b92467
Kornienko Alexey <korn.alexey@gmail.com>
2022-08-22 09:17:14 +0300

    home

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const requestButton = document.querySelector('.send-request');
+
+    requestButton.addEventListener('click', (e) => {
+        const name = document.querySelector('#validationName').value;
+        const telephone = document.querySelector('#validationTelephone').value;
+        if (name && telephone) {
+            e.preventDefault();
+            sendRequest(name, telephone);
+        }
+        
+    }) 
+
+    const sendRequest = (name, telephone) => {
+        console.log(name, telephone);
+    }
+
+});
