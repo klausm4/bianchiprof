243c5aec3ba48e8c4f226aa8f5a18952ed3b03f0
Kornienko <korn.alexey@gmail.com>
2022-10-28 15:59:30 +0300

    work

--- src/js/modules/navbar.js
+++ src/js/modules/navbar.js
@@ -1,12 +1,12 @@
 function navbar() {
 
     window.addEventListener('scroll', e => {
-        const navbarClasslist = document.querySelector('#navbar').classList;
+        const navbarClassList = document.querySelector('#navbar').classList;
         const active_class = 'navbar-opacity';
         if (scrollY > 100) {
-            navbarClasslist.add(active_class);
+            navbarClassList.add(active_class);
         } else {
-            navbarClasslist.remove(active_class);
+            navbarClassList.remove(active_class);
         };
     })
 };
